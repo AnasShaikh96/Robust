8c5f71b2d46513c1c89fb86a5835667d0eb034a2
Anas Shaikh <anas.shaikh@graymatrix.com>
2022-04-09 13:55:11 +0530

    uodated hero image

---
+++ src/views/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar';
+import Towers from "../../assets/images/towers.jpg";
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <div className='hero-wrapper'>
+                <div className="container">
+                    <div className='hero-title'>
+                        {/* <h1>What We Do?</h1> */}
+                    </div>
+                </div>
+            </div>
+            <div>
+                hello
+            </div>
+        </>
+    )
+}
