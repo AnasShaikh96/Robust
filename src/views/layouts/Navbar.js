9df28c8ee30781a9f27113dec8b85a73aa8a48ce
Anas Shaikh <anas.shaikh@graymatrix.com>
2022-06-11 13:16:29 +0530

    added fa

--- src/views/layouts/Navbar.js
+++ src/views/layouts/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
                 <div className="container">
                     <a className="navbar-brand" href="/">Robust</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="fa fa-bars text-light"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul className="navbar-nav">
