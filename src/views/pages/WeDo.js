120edd6f3b58526867475fc1aae66478c5c00f03
Anas Shaikh <anas.shaikh@graymatrix.com>
2022-06-20 20:09:20 +0530

    added what we do section

---
+++ src/views/pages/WeDo.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+export default function WeDo() {
+    return (
+        <>
+            <div className="container" id="wwd">
+                <h2 className='my-5 fw-bold'>What we do?</h2>
+                <div className="row">
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-building'></i>
+                                </h1>
+                                <h5>Structural Designing &amp; Audit</h5>
+                                <small>
+                                    Structural Audit is an examination of the overall healthof a building to ensure that it is safe,risk-free and habitable.
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-magnifying-glass-dollar'></i>
+                                </h1>
+                                <h5>Estimation &amp; Cost</h5>
+                                <small>
+                                    Estimation (or estimating) is the process of finding an estimate or approximation.
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-hammer'></i>
+                                </h1>
+                                <h5>Repairs &amp; Rehabilitation</h5>
+                                <small>Repair and Rehab refers to modification of a structure, partly or wholy which is damaged in appearance or serviceability.</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-ruler-combined'></i>
+                                </h1>
+                                <h5>Interior Designing</h5>
+                                <small>Enhancing the interior of a building to achieve a healthier and more aesthetically pleasing environment for the people.</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-paintbrush'></i>
+                                </h1>
+                                <h5>Painting</h5>
+                                <small>Painting is done to improve the appearance of a building and to protect it from damage by water, corrosion, insects and mould</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 my-3">
+                        <div className="card  border-0">
+                            <div className="card-body">
+                                <h1>
+                                    <i className='fa fa-droplet'></i>
+                                </h1>
+                                <h5>Water Proofing</h5>
+                                <small>
+                                Waterproofing is an aspect of creating a building envelope, which is a controlled environment. 
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
